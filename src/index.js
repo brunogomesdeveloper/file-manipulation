0da01584968b35ea235d0dbb55f165293a059542
Bruno Gomes <brunogomesdeveloper@gmail.com>
2022-02-04 20:17:35 -0300

    add packs

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const getMP3Duration = require('get-mp3-duration')
+const pdf = require('pdf-parse')
+const { getVideoDurationInSeconds} = require('get-video-duration')
+var Readable = require('stream').Readable
+
+
