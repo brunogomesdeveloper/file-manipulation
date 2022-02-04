f4b801b0d12b9fb04614f723ec7539b414b0c8f5
Bruno Gomes <brunogomesdeveloper@gmail.com>
2022-02-04 20:35:48 -0300

    creating function getDuration

--- src/index.js
+++ src/index.js
@@ -4,3 +4,34 @@ const { getVideoDurationInSeconds} = require('get-video-duration')
 var Readable = require('stream').Readable
 
 
+module.exports = { 
+    getDuration  : async (buffer, extesion) => {
+        try {
+            let duration = 0
+            switch (extesion) {
+                case "mp3":
+                    duration = getMP3Duration(buffer)   
+                break;
+                case "pdf":
+                    const pdfParse = await pdf(buffer)
+                    duration = (pdfParse.text.split(" ").length / 130) * 60
+                break;
+                case "mp4":
+                case "mov":
+                case "m4v" : 
+                    let stream = new Readable()
+                    stream.push(buffer)
+                    stream.push(null)
+                    duration =  await getVideoDurationInSeconds(stream)
+                default:
+                    break;
+            }
+            return duration    
+        } catch (error) {
+            
+        }
+    }
+
+}
+
+
